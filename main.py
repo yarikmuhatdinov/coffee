24f37bd3dc088e311dd81f1674f8b8ce42bbb6cb
yarikmuhatdinov <yarikmuhatdinov@yandex.ru>
2022-01-24 06:33:18 +0300

    first edit

--- main.py
+++ main.py
@@ -2,18 +2,59 @@ import sqlite3
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class addEditCoffeeForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+
+        self.pushButtonEdit.clicked.connect(self.start_edit)
+
+    def start_edit(self):
+        cur = self.connection.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("SELECT * FROM films WHERE id=?",
+                             (item_id := self.spinBox.text(),)).fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
 
 
 class DBSample(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+
         self.connection = sqlite3.connect("coffee.sqlite")
+
         self.pushButton.clicked.connect(self.select_data)
+
         self.textEdit.setPlainText("SELECT * FROM coffee")
+
         self.select_data()
+
+        self.pushButtonEdit.clicked.connect(self.addEdit)
+
+        self.addEditCoffee = addEditCoffeeForm()
+
+    def addEdit(self):
+        self.addEditCoffee.show()
 
     def select_data(self):
         # Получим результат запроса,
